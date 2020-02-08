870fb66d9de1ee7e818f4ef14ccbd86e58829a94
Damely Tineo <damely@Damelys-MBP.fios-router.home>
2020-02-08 18:56:40 -0500

    Get user zip-code. Save data to zip_code variable for later use.

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,6 +3,7 @@ class FluVaccination::CLI
   def call
     puts "Welcome to the Flu Vaccination CLI App! Protect yourself and others, get vaccinated!"
     puts "For a list of places offering free vaccination against the flu near you enter your zip-code."
+    zip_code = gets.chomp
   end
 
 end
