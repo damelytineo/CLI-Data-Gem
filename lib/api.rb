de2bbed9bf49b623a94ffd92e75d67c91a787e3b
Damely Tineo <damely@Damelys-MBP.fios-router.home>
2020-02-08 19:45:40 -0500

    Used HTTParty to access data filtered from API call by appending zip_code to BASE_URL.

--- lib/api.rb
+++ lib/api.rb
@@ -2,4 +2,9 @@ class FluVaccination::API
 
   BASE_URL = 'https://data.cityofnewyork.us/resource/w9ei-idxz.json?service_category=Vaccines&service_type=Flu%20Vaccine%20(Influenza)'
 
+  def filtered_by_zip(zip_code)
+    HTTParty.get(BASE_URL + "&zip_code=#{zip_code}")
+  end
+
+
 end
