2afcaf4174106faa98129931d268ffef5f68be4a
Damely Tineo <damely@Damelys-MBP.fios-router.home>
2020-02-09 12:42:09 -0500

    Turn API call data into instances of FluVaccination class. Mass assign data (hash of key-values) to defined instance variables. Call NoMethodError if instance var is not defined. Store data in class var @@all for later use in CLI - call via all_locations method.

--- lib/flu_vaccination.rb
+++ lib/flu_vaccination.rb
@@ -1,5 +1,21 @@
 class FluVaccination::FluVaccination
+  attr_accessor :facility_name, :address, :borough, :zip_code, :phone, :walk_in, :children, :more_information
 
+  @@all = []
 
+  def initialize(location)
+    location.each {|key, value|
+      begin
+        self.send(("#{key}="), value)
+      rescue NoMethodError
+      end
+    }
+    @@all << self
+  end
+
+  def self.all_locations
+    @@all
+  end
 
 end
+#Turn API call data into instances of FluVaccination class. Mass assign data (hash of key-value pairs) to defined instance variables. Call NoMethodError if instance var is not defined. Store in class var @@all for later use in CLI - access this info via all_locations method.
